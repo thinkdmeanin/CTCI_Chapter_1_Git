d9868db73aa03126045a8f5b63539f9f2cf3b3ee
thinkdmeanin <zheming.xin.sjtu@gmail.com>
2013-10-25 15:40:09 -0400

    v0.1 add comments and change return variable name to result

--- CTCI_Chapter_1/src/TestBench_1_1.java
+++ CTCI_Chapter_1/src/TestBench_1_1.java
@@ -1,25 +1,29 @@
 public class TestBench_1_1 {
-	public static boolean checkUnique (String s) {
-		if (s == null) {
+	public static boolean checkUnique (String s) {	// function: input a string to see if all letters are unique
+		if (s == null) {	// if empty => unique;  do this to remove null-pointer exception
 			return true;
 		}
-		char[] c = s.toCharArray();
-		int[] checkList = new int[128];
-		boolean flag = true;
-		for (char letter : c) {
-			if (checkList[letter] != 0) {
-				flag = false;
-				return flag;
+		
+		char[] c = s.toCharArray();			// convert to char array
+		int[] checkList = new int[128];		// ASCII list storing the appearance frequency of ASCII chars in String 
+		boolean result = true;				// initialize result to be true
+		
+		for (char letter : c) {			// scan through the string 
+			if (checkList[letter] != 0) {	// if this char repeats 
+				result = false;		// return false
+				return result;
 			}
-			checkList[letter]++;
+			checkList[letter]++;		// otherwise update checkList
 		}
-		return flag;
+		return result;		// reach here means all unique and return true
 	}
 	
-	public static void main(String[] arg) {
-		String[] testList = {null, "abcde", "aaaaaaa", "abcdc", "1233", "0000000"};
+	public static void main(String[] arg) {	// Testbench method
+		String[] testList = {null, "abcde", "aaaaaaa", "abcdc", "1233", "0000000"};	
+			// test empty, successive repetition, numbers
 		System.out.println("Now start checking uniquenes of Strings:");
-		for (String testWord : testList) {
+			// Info output
+		for (String testWord : testList) {	// test all the words
 			System.out.printf("Does \"%s\" only contain unique characters? %b\n", testWord, TestBench_1_1.checkUnique(testWord));
 		}
 	}
