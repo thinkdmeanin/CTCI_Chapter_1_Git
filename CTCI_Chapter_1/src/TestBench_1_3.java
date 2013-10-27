980e023a1e5ba6fed3e5997b4757aa296e11dd67
thinkdmeanin <zheming.xin.sjtu@gmail.com>
2013-10-27 15:22:27 -0400

    problem 1-3 v0.1 time n space n added comments

--- CTCI_Chapter_1/src/TestBench_1_3.java
+++ CTCI_Chapter_1/src/TestBench_1_3.java
@@ -2,31 +2,32 @@ import java.util.Arrays;
 
 public class TestBench_1_3 {
 	public static String removeDup (String str) {
-		if (str == null) return str;
+		if (str == null) return str;	// avoid nullPointer Exception
 		char[] cArray = str.toCharArray();
-		int[] lastAppear = new int[256];
+		int[] lastAppear = new int[256];	// record the index this ASCII char last appears in str
 		int len = str.length();
-		int val = -1;
-		int end = 0;
+		int val = -1;	// store the int casted from ASCII
+		int end = 0;	// store the next position to put in new unique char
 		
 		for (int i = 0; i < lastAppear.length; ++i) {
-			lastAppear[i] = -1;
+			lastAppear[i] = -1;		// reset lastAppear array to -1
 		}
 		
 		for (int i = 0; i < len; ++i) {
-			val = cArray[i];
-			if (lastAppear[val] != -1) {
-				cArray[lastAppear[val]] = '\0';
+			val = cArray[i];	// cast ASCII char to int
+			if (lastAppear[val] != -1) {	// if appears again
+				cArray[lastAppear[val]] = '\0';		// clear the pre-appear char in cArray
 			}
-			lastAppear[val] = i;
+			lastAppear[val] = i;	// anyway record the only and newest appear position of such char
 		}
 		
 		for (int i = 0; i < len; ++i) {
-			if (cArray[i] != '\0') cArray[end++] = cArray[i];
+			if (cArray[i] != '\0') cArray[end++] = cArray[i];	// record each unique char to the beginning string tail
 		}
 		
-		String result = null;
-		if (end > 0)  result = new String(Arrays.copyOfRange(cArray, 0, end));
+		String result = null;	// if all char is '\0'
+		if (end > 0)  result = new String(Arrays.copyOfRange(cArray, 0, end));	// if not all char is '\0', copy to a String
+			// be very careful that: Arrays.copyOfRange(<arrayName>, from, to) => from: 1 to: 4 copys [1],[2],[3] element!!
 		return result;
 	}
 	
