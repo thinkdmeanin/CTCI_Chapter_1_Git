611462e483795339927f0508384344cebcc8074f
thinkdmeanin <zheming.xin.sjtu@gmail.com>
2013-10-27 11:29:00 -0400

    problem 1-2 v0.0 space n time n

--- CTCI_Chapter_1/src/TestBench_1_2.java
+++ CTCI_Chapter_1/src/TestBench_1_2.java
@@ -1,5 +1,5 @@
 public class TestBench_1_2 {
-	public static String reverseCStr (String str) {
+	public static String reverseCStr (String str) {	
 		if (str == null) return str;
 		char[] cArray = str.toCharArray();
 		int len = str.length();
@@ -9,19 +9,22 @@ public class TestBench_1_2 {
 			cArray[i] = cArray[len-2-i];
 			cArray[len-2-i] = temp;
 		}
-		new String(cArray);
-		return 
+		String result = new String(cArray);  // String(Char[] ) works while Char[].toString() return mass
+//		String result = cArray.toString();
+		return  result;
 	}
 	
 	public static void main (String[] args) {
 		String[] testStr = {"abcd\0", "abcde\0", "123\0", "ab1p4\0", null};
-		System.out.printf("Test Strings: \t\t Reverse Result: \n");
+		System.out.printf("Test Strings: \t\t Reverse Result: \n");	
+			// \t is a good way to format output, but take care of word length diff
 		String temp = null;
 		for (String str : testStr) {
 //			temp = reverseCStr(str);
 //			char[] ctemp = {'a', 'b', 'c', 'd'};
 //			temp = new String(ctemp);
-			System.out.printf("%s \t\t\t %s\n", str, temp);
+			System.out.printf("%s \t\t\t %s\n", str, reverseCStr(str));
+				// \t is a good way to format output, but take care of word length diff
 		}
 	}
 }
