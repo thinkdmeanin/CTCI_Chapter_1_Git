c297e86a0b5e4cea0fafeea89364e73b1fa2847b
thinkdmeanin <zheming.xin.sjtu@gmail.com>
2013-10-27 11:39:49 -0400

    problem 1-2 v1.0 half work

---
+++ CTCI_Chapter_1/src/TestBench_1_2_Answer.java
@@ -0,0 +1,17 @@
+public class TestBench_1_2_Answer {
+	public static void reverse(char* str) {
+//		char *end = str;
+//		char tmp;
+//		if(str) {
+//			while (*end) {
+//				++end
+//			}
+//			--end;
+//			while (str < end) {
+//				temp = *str;
+//				*str++ = *end;
+//				*end-- = tmp;
+//			}
+//		}		
+	}
+}
