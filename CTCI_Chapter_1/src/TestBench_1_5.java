f76de1cf5fa7e521669b352587936a0e755e725e
thinkdmeanin <zheming.xin.sjtu@gmail.com>
2013-10-27 19:36:21 -0400

    1-5 me t n s n

---
+++ CTCI_Chapter_1/src/TestBench_1_5.java
@@ -0,0 +1,20 @@
+public class TestBench_1_5 {
+	public static String removeSpace (String str) {
+		if (str == null) return str;
+		String[] words = str.split("\\s+");	// split
+		
+		StringBuffer result = new StringBuffer();
+		for (String word : words) {
+			result.append(word);	// and combine
+		}
+		return result.toString(); // return converted
+	}
+	
+	public static void main (String[] args) {
+		String[] testStr = {null, "abc", "a b c", "a    b c", "a \t\tb \t \t"};
+		System.out.printf("Tested String:\tNo Space String:\n");
+		for (String str : testStr) {
+			System.out.printf("%s\t\t%s\n", str, removeSpace(str));
+		}
+	}
+}
