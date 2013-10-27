6d3bab30a6216f7971a21d0ce406004c9c4606b6
thinkdmeanin <zheming.xin.sjtu@gmail.com>
2013-10-27 19:23:15 -0400

    1-4 bk t n s n == so stupid..

---
+++ CTCI_Chapter_1/src/TestBench_1_4_Answer.java
@@ -0,0 +1,27 @@
+public class TestBench_1_4_Answer {
+	public static boolean anagram(String s, String t) {
+		if (s.length() != t.length()) return false;
+		int[] letters = new int[256];
+		int num_unique_chars = 0;
+		int num_completed_t = 0;
+		char[] s_array = s.toCharArray();
+		for (char c : s_array) {
+			if (letters[c] == 0) ++num_unique_chars;
+			++letters[c];
+		}
+		for (int i = 0; i < t.length(); ++i) {
+			int c = (int) t.charAt(i);
+			if (letters[c] == 0) {
+				return false;
+			}
+			--letters[c];
+			if (letters[c] == 0) {
+				++num_completed_t;
+				if (num_completed_t == num_unique_chars) {
+					return i == t.length() -1;
+				}
+			}
+		}
+		return false;
+	}
+}
