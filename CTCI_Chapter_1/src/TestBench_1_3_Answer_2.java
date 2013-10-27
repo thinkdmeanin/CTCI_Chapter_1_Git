4e856aa70fe14f93fc45cafbc164ee1f45c80977
thinkdmeanin <zheming.xin.sjtu@gmail.com>
2013-10-27 16:50:05 -0400

    problem v1.0 time n space 1 but I cannot compile them..

--- CTCI_Chapter_1/src/TestBench_1_3_Answer_2.java
+++ CTCI_Chapter_1/src/TestBench_1_3_Answer_2.java
@@ -1,20 +1,20 @@
-public class TestBench_1_3_Answer {
-	public static void removeDuplicatesEff (char[] str) {
+
+public class TestBench_1_3_Answer_2 {
+	public static void removeDuplicates (char[] str) {
 		if (str == null) return;
 		int len = str.length;
 		if (len < 2) return;
-		boolean[] hit = new boolean[256];
-		for (int i = 0; i < 256; ++i) {
-			hit[i] = false;
-		}
-		hit[str[0]] = true;
+		
 		int tail = 1;
+		
 		for (int i = 1; i < len; ++i) {
-			if (!hit[str[i]]) {	
-				// find every new unique and copy to updated string tail and move on 
+			int j;
+			for (j = 0; j < tail; ++j) {
+				if (str[i] == str[j]) break;
+			}
+			if (j == tail) {
 				str[tail] = str[i];
 				++tail;
-				hit[str[i]] = true;
 			}
 		}
 		str[tail] = 0;
@@ -26,8 +26,10 @@ public class TestBench_1_3_Answer {
 		char[] tmp = null;
 		for (String str : testStr) {
 			tmp = str.toCharArray();
-			removeDuplicatesEff(tmp);
+			removeDuplicates(tmp);
 			System.out.printf("%s\t\t%s\n", str, new String(tmp));
 		}
 	}
 }
+
+
