90a0a07a402920e33bd4a4861bca04968a643ac4
thinkdmeanin <zheming.xin.sjtu@gmail.com>
2013-10-29 10:01:06 -0400

    1-8 me t n s n

--- CTCI_Chapter_1/src/TestBench_1_6_Answer.java
+++ CTCI_Chapter_1/src/TestBench_1_6_Answer.java
@@ -5,8 +5,11 @@ public class TestBench_1_6_Answer {
 			int last = n-1-layer;
 			for (int i = first; i < last; ++i) {
 				int offset = i - first;
-				int top = matrix[first][i]; // save top
+					// me: offset sounds better than shift
+				int top = matrix[first][i]; // save top	
+					// me: use top instead of tmp is better
 				// left -> top
+					// me: show which rotation for each assignment
 				matrix[first][i] = matrix[last-offset][first];
 				// bottom -> left
 				matrix[last-offset][first] = matrix[last][last - offset];
